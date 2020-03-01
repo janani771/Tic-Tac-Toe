bdee2cfe63430e5b7e7bd3f5285dd376b6321f8b
jananin <janani.natarajan@metropolia.fi>
2020-03-01 11:57:16 +0200

    Now, the player 1 and player 2 textview will be replaced respectively by the user input. And added some design attributes.

--- app/src/main/java/com/example/tictactoe/Game.java
+++ app/src/main/java/com/example/tictactoe/Game.java
@@ -3,8 +3,8 @@ package com.example.tictactoe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Game extends AppCompatActivity {
@@ -14,6 +14,21 @@ public class Game extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message2 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE2);
+
+
+        TextView playerOneName = findViewById(R.id.P1);
+        playerOneName.setText(message + ":   ");
+
+        TextView playerTwoName = findViewById(R.id.P2);
+        playerTwoName.setText(message2 + ":   ");
+
+    }
+    public void quit(View view){
+        Intent quit = new Intent(this, quit.class);
+
+        startActivity(quit);
     }
 
 }
--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.tictactoe2.MESSAGE";
+    public  static final String EXTRA_MESSAGE2 = "com.example.tictactoe2.MESSAGE2";
     public Button instruction;
 
     @Override
@@ -33,12 +36,17 @@ public class MainActivity extends AppCompatActivity {
     public void play(View view) {
         Intent game = new Intent(this, Game.class);
 
+        EditText nameOne = (EditText) findViewById(R.id.player1name);
+        EditText nameTwo = (EditText) findViewById(R.id.player2name);
+
+        String message = nameOne.getText().toString();
+        String message2 = nameTwo.getText().toString();
+
+        game.putExtra(EXTRA_MESSAGE, message);
+        game.putExtra(EXTRA_MESSAGE2, message2);
+
+
         startActivity(game);
     }
 
-    public void quit(View view) {
-        Intent quit = new Intent(this, DisplayMessageActivity.class);
-
-        startActivity(quit);
-    }
 }
